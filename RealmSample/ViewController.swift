7701c8a5acf889473645250aeebc5753ad04eed7
Reo Yoshida <reoy@r.recruit.co.jp>
2015-08-04 18:40:14 +0900

    initial commit

---
+++ RealmSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmSample
+//
+//  Created by Reo Yoshida on 2015/08/04.
+//  Copyright (c) 2015年 Reo Yoshida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
